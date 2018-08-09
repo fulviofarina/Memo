c520d4a9ea72648e89f98d8dbaf865894adafb3e
Fulvio Farina <fulviofarina@gmail.com>
2018-08-08 21:04:39 -0400

    Add project files.

---
+++ Memo/Memo.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Memo.ino
+ Created:	8/8/2018 21:04:32
+ Author:	Fulvio
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
